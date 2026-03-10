755d6e53a4fc0e2e45a80786e54b1bc0174e61db
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-03-10 11:55:03 +0000

    chore(example-app): Port camera screen from capacitor-testapp

--- example-app/src/App.tsx
+++ example-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import Camera from './pages/Camera';
 import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
@@ -41,6 +42,9 @@ const App: React.FC = () => (
       <IonRouterOutlet>
         <Route exact path="/home">
           <Home />
+        </Route>
+        <Route exact path="/camera">
+          <Camera />
         </Route>
         <Route exact path="/">
           <Redirect to="/home" />
