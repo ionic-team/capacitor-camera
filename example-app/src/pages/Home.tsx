2ca663fff70c381e77c13bc1c58ddca171b8eae2
Pedro Bilro <pedro.gustavo.bilro@outsystems.com>
2026-03-10 16:55:19 +0000

    chore: initial example app (#3)
    * chore: add example app with ionic start
    
    * chore: Update ignore files for linter
    
    * chore: update example app dependencies
    
    * chore: Add Android native app
    
    * chore: Add iOS native app
    
    * chore: ios auto-update
    
    * chore(example-app): Port camera screen from capacitor-testapp
    
    * chore(example-app): Add native permissions
    
    * chore(example-app): Add burguer menu

---
+++ example-app/src/pages/Home.tsx
@@ -0,0 +1,36 @@
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank; TODO - This page will have content in a future PR</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
