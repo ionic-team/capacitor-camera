bff0cc560124e73b451ffee593f927902a5d90e0
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-04-10 11:57:22 +0100

    chore: Update native Android and iOS dependencies

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
-        .package(url: "https://github.com/ionic-team/ion-ios-camera.git", branch: "main") // TODO: update to a stable release when available
+        .package(url: "https://github.com/ionic-team/ion-ios-camera.git", from: "1.0.0") 
     ],
     targets: [
         .target(
