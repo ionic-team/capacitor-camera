dbfd310e051361d18a4c9f1954c49705f36e06ad
Rui Mendes <rui.moreira.mendes@outsystems.com>
2026-07-30 16:38:47 +0100

    fix(ios): bump ion-ios-camera to 1.0.5 for iOS 27 UIScene compliance

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "9.0.0-alpha.5"),
-        .package(url: "https://github.com/ionic-team/ion-ios-camera.git", from: "1.0.4")
+        .package(url: "https://github.com/ionic-team/ion-ios-camera.git", from: "1.0.5")
     ],
     targets: [
         .target(
